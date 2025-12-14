4f6e84ef5a5771bb5043c5156f6ab58dd74a30c3
Ubuntu <saifmk10@agent-vm.evz5qau33ztu5hexbgdkxhun4c.lx.internal.cloudapp.net>
2025-12-14 16:31:15 +0000

    [UPDATED DOCKER BE] - updated the docker logic to fetch the data from tapi

--- Stock Automation/fetchingStock.py
+++ Stock Automation/fetchingStock.py
@@ -1,7 +1,14 @@
 import requests
 import time , csv
+import os
+
+stockName = "YESBANK"
 
 url = "https://the-chat-app-api-git-main-saifmks-projects.vercel.app/api/searchedapi.py?symbol=YESBANK"
+
+
+HEADER = ["EXTRACTED_DATE","EXTRACTED_TIME","STOCK_NAME","EXTRACTED_PRICE"]
+
 
 try : 
     while url :
@@ -9,21 +16,29 @@ try :
         print("response : " , response.status_code)
         print(response.json())
 
-        currentTime = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
+        date = time.strftime("%d-%m-%Y", time.localtime())
+        currentTime = time.strftime("%H:%M:%S", time.localtime())
         data = response.json()
         # print(price["stockPrice"])
 
         price = float(data["stockPrice"])
         name = data["stockName"]
+        # file_empty = os.makedirs("/data/input.csv", exist_ok=True) and os.stat("/data/input.csv").st_size == 0
+        file_exists = os.path.isfile("/data/input.csv")
 
 
         # /app/input.csv ---> use this when running the code on the local docker also keeo cahanging the path
         # ./input.csv  ---> using this in the azure vm change if any error comes
+        # /data/input.csv
         with open("/data/input.csv" , "a" , newline="") as f:
             writer = csv.writer(f)
-            writer.writerow([currentTime , name ,price])
-        
-        time.sleep(10)
+
+            if not file_exists : 
+                writer.writerow(HEADER)
+
+            writer.writerow([date,currentTime,name,price])
+        time.sleep(20)
+        # print(time.now())
 
 
 except requests.exceptions.RequestException as e:
