f90e7911841bb28d4317d11e0f349457fe69bbfb
Ubuntu <saifmk10@agent-vm.evz5qau33ztu5hexbgdkxhun4c.lx.internal.cloudapp.net>
2025-12-18 08:10:28 +0000

    [UPDATED STRUCTURE] - changed the structure of the code and cron jobs

---
+++ Stock_Automation/gmailSubscription.py
@@ -0,0 +1,22 @@
+import yagmail
+import collectedDataAnalysis
+
+yag = yagmail.SMTP('saifmohasaif216@gmail.com' , 'lefw fwqi eqnp lvvb') 
+
+content  = collectedDataAnalysis.analysisPandas("/data/YESBANK.csv")
+
+gmails = [
+    'saifmkpvt@gmail.com' ,
+    # 'mohdshyni@gmail.com' 
+]
+password = 'lefw fwqi eqnp lvvb'
+subject = "Daily stock price analysis summary"
+
+try : 
+    yag.send( gmails , subject , content)
+    print(f"MAIL SENT TO {gmails}")
+except Exception as error : 
+    print(f"ERROR IN AUTHENTICATION : {error}")
+
+
+# this is currently a test code not stuctured properly so needs to be updated with addition of functions
