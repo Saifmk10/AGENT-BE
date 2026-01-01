ac0bfed6e2574a2878a5bb05dd8f2e3983277b25
saif <saifmk10@saifagent.taile98f28.ts.net>
2026-01-01 09:05:01 +0000

    [UPDATED PIPELINE] - updated structural init

--- Stock_Automation/Data_fetching_from_db/connection.py
+++ Stock_Automation/Data_fetching_from_db/connection.py
@@ -1,9 +1,18 @@
 # file that holds the credential and all the details that contains access points to the db
-
+import os
 import firebase_admin
 from firebase_admin import credentials , firestore
 
-Credentails = credentials.Certificate('D:/PROJECTS/AGENT-BE/the-chat-44e8e-firebase-adminsdk-fbsvc-7f501ef3f6.json')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+cred_path = os.path.join(
+    BASE_DIR,
+    "the-chat-44e8e-firebase-adminsdk-fbsvc-7f501ef3f6.json"
+)
+
+
+
+Credentails = credentials.Certificate(cred_path)
 
 app = firebase_admin.initialize_app(Credentails)
 
--- Stock_Automation/run.py
+++ Stock_Automation/run.py
@@ -2,6 +2,8 @@
 #any execution thay need to happen on the fetchingStock.py needs to happen in this file ONLY
 
 from Data_collection_automation.fetchingStock import main
+from Data_collection_automation.collectedDataAnalysis import init_storage
 
 if __name__ == "__main__":
+    init_storage() 
     main()
