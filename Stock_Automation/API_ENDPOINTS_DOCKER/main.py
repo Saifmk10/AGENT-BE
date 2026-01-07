a566ffbf5ed1397cd266808ef711e846c56bcba3
Saifmk10 <saifmkpvt@gmail.com>
2026-01-07 23:36:42 +0530

    [API UPDATED] - updated the api to run in the server making , introducing monolithinc system

--- Stock_Automation/API_ENDPOINTS_DOCKER/main.py
+++ Stock_Automation/API_ENDPOINTS_DOCKER/main.py
@@ -1,13 +1,19 @@
+# file that is responsible for making the api be accessable with the help of the fastapi
+# these end points will be make public with the help of cloudflare tunnel
+
 from fastapi import FastAPI
 from searchedapi import userSearchedStockPrice
 
 app = FastAPI()
 
+# default api endpoint to test api status
 @app.get("/")
 
 def read_root():
     return{"STATUS" : "Api is running"}
 
+
+# api end point where the users can search for a particular stock   [SEARCH OPTION]
 @app.get("/stock/{symbol}")
 
 def get_stock(symbol : str):
